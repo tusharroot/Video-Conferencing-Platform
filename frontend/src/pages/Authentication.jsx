88c5463bf16faa3a86ab53e15ad65939ae5fe705
Tushar Patel <tusharpatel3217@gmail.com>
2025-08-01 12:39:48 -0500

    Push latest working version of the project

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -14,7 +14,9 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { AuthContext } from "../contexts/AuthContext";
 import Snackbar from "@mui/material/Snackbar";
-
+import HomeIcon from "@mui/icons-material/Home";
+import { IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 function Copyright(props) {
   return (
     <Typography
@@ -42,120 +44,145 @@ export default function Authentication() {
   const [error, setError] = React.useState();
   const [message, setMessage] = React.useState();
 
-  const [formstate, setFormstate] = React.useState(0); 
+  const [formstate, setFormstate] = React.useState(0);
 
   const [open, setOpen] = React.useState();
 
-  const {handlelogin, handleregister} = React.useContext(AuthContext);
+  const { handlelogin, handleregister } = React.useContext(AuthContext);
   let handleAuth = async () => {
     try {
       if (formstate === 0) {
-        let result = await handlelogin(username,password);
-        
+        let result = await handlelogin(username, password);
       }
       if (formstate === 1) {
         let result = await handleregister(name, username, password);
         console.log(result);
         setMessage(result);
         setOpen(true);
-        SetUsername("")
-        setError("")
-        setFormstate(0)
-        SetPassword("")
+        SetUsername("");
+        setError("");
+        setFormstate(0);
+        SetPassword("");
       }
     } catch (error) {
       let message = error.response.data.message;
       setError(message);
     }
   };
-
+  const routeTo = useNavigate();
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh",alignItems:"center",justifyContent:"center" }}>
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+    <>
+      <IconButton
+        style={{ height: "50px", width: "50px" }}
+        onClick={() => {
+          routeTo("/");
+        }}
+      >
+        <HomeIcon style={{ height: "50px", width: "50px" }} />
+      </IconButton>
+      <ThemeProvider theme={defaultTheme}>
+        <Grid
+          container
+          component="main"
+          sx={{
+            height: "100vh",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CssBaseline />
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={5}
+            component={Paper}
+            elevation={6}
+            square
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
 
-            <div>
-              <Button
-                variant={formstate == 0 ? "contained" : ""}
-                onClick={() => setFormstate(0)}
-              >
-                Sign In
-              </Button>
-              <Button
-                variant={formstate == 1 ? "contained" : ""}
-                onClick={() => setFormstate(1)}
-              >
-                Sign Up
-              </Button>
-            </div>
+              <div>
+                <Button
+                  variant={formstate == 0 ? "contained" : ""}
+                  onClick={() => setFormstate(0)}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  variant={formstate == 1 ? "contained" : ""}
+                  onClick={() => setFormstate(1)}
+                >
+                  Sign Up
+                </Button>
+              </div>
 
-            <Box component="form" noValidate sx={{ mt: 1 }}>
-              {formstate == 1 ? (
+              <Box component="form" noValidate sx={{ mt: 1 }}>
+                {formstate == 1 ? (
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="Fullname"
+                    label="Fullname"
+                    value={name}
+                    name="Fullname"
+                    autoFocus
+                    onChange={(e) => SetName(e.target.value)}
+                  />
+                ) : (
+                  <></>
+                )}
                 <TextField
                   margin="normal"
                   required
                   fullWidth
-                  id="Fullname"
-                  label="Fullname"
-                  value={name}
-                  name="Fullname"
+                  id="username"
+                  label="username"
+                  name="username"
+                  value={username}
                   autoFocus
-                  onChange={(e) => SetName(e.target.value)}
+                  onChange={(e) => SetUsername(e.target.value)}
                 />
-              ) : (
-                <></>
-              )}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="username"
-                name="username"
-                value={username}
-                autoFocus
-                onChange={(e) => SetUsername(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                value={password}
-                id="password"
-                onChange={(e) => SetPassword(e.target.value)}
-              />
-              <p style={{color:"red"}}>{error}</p>
-              <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                onClick={handleAuth}
-              >
-                {formstate===0?"LogIn":"Register"}
-              </Button>
-              <Copyright sx={{ mt: 5 }} />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  value={password}
+                  id="password"
+                  onChange={(e) => SetPassword(e.target.value)}
+                />
+                <p style={{ color: "red" }}>{error}</p>
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  onClick={handleAuth}
+                >
+                  {formstate === 0 ? "LogIn" : "Register"}
+                </Button>
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-      <Snackbar open={open} autoHideDuration={4000} message={message} />
-    </ThemeProvider>
+        <Snackbar open={open} autoHideDuration={4000} message={message} />
+      </ThemeProvider>
+    </>
   );
 }
